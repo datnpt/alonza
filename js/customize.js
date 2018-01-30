f9881f3a9e22d881c8ff8f4d8d2a3771fab015da
DESKTOP-G46QH2B\EVA-DatNPT <npthanhdat@gmail.com>
2018-01-30 15:56:06 +0700

    update float-btn

--- js/customize.js
+++ js/customize.js
@@ -1,4 +1,34 @@
 (function($) {
-	
+	// scrollBottom
+	function scrollBottom() { 
+		return $(this).scrollTop() + $(this).height();
+	};
 
+	// hide/show float_btn
+	function hideFloatBtn() {
+		var sb = scrollBottom(),
+			btn = $('.float_btn'),
+			pos = $('#form').offset().top + (btn.outerHeight() / 2);
+
+		if(sb >= pos) {
+			btn.fadeOut();
+		} else {
+			btn.fadeIn();
+		}
+	}
+
+	// window load
+	$(window).load(function() {
+		hideFloatBtn();
+	});
+
+	// window resize
+	$(window).resize(function() {
+		hideFloatBtn();
+	});
+
+	// window scroll
+	$(window).scroll(function() {
+		hideFloatBtn();
+	});
 })(jQuery);
